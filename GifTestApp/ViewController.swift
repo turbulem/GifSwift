67d865385f7c5d251a212f3797f9ba1c725e5bac
Sergey Lem <lemsergey@gmail.com>
2019-01-21 12:45:41 +0000

    Update test app code

--- GifTestApp/ViewController.swift
+++ GifTestApp/ViewController.swift
@@ -6,7 +6,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let file = GifFile(path: Bundle.main.url(forResource: "cat", withExtension: "gif")!) {
+        if let file = GifFile(path: Bundle.main.path(forResource: "cat", ofType: "gif")!) {
             debugPrint("Image has size: \(file.size) and contains \(file.imagesCount) images")
         }
     }
